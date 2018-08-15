9d632c86dccbba963e75e8e9a43f8e5e8b91b9d6
Adytzu <40206851+Adytzuma@users.noreply.github.com>
2018-08-15 15:41:40 +0300

    Update paginator.py

--- cogs/utils/paginator.py
+++ cogs/utils/paginator.py
@@ -428,7 +428,7 @@ class HelpPaginator(Pages):
         self.embed.title = self.title
 
         if hasattr(self, '_is_bot'):
-            value ='For more help, join the official bot Support server: [Here]( https://discord.gg/B35rdCv )'
+            value ='For more help, join the official bot Support server: [Here]( https://discord.gg/vqquZAj )'
             self.embed.add_field(name='Support', value=value, inline=False)
 
         self.embed.set_footer(text=f'Use "{self.prefix}help command" for more info on a command.')
